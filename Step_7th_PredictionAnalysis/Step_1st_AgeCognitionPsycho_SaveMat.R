12caa32f663e689f6e082dfd64985f374233cfcb
Zaixu Cui <zaixucui@gmail.com>
2019-07-04 10:38:22 -0400

    codes

--- Step_7th_PredictionAnalysis/Step_1st_AgeCognitionPsycho_SaveMat.R
+++ Step_7th_PredictionAnalysis/Step_1st_AgeCognitionPsycho_SaveMat.R
@@ -17,13 +17,9 @@ Motion <- (Motion_Rest + Motion_NBack + Motion_Emotion)/3;
 F1_Exec_Comp_Res_Accuracy <- AllInfo$F1_Exec_Comp_Res_Accuracy;
 F3_Memory_Accuracy <- AllInfo$F3_Memory_Accuracy;
 F2_Social_Cog_Accuracy <- AllInfo$F2_Social_Cog_Accuracy;
-F1_Slow_Speed <- AllInfo$F1_Slow_Speed;
-F2_Fast_Speed <- AllInfo$F2_Fast_Speed;
-Overall_Speed <- AllInfo$Overall_Speed;
-F3_Memory_Speed <- AllInfo$F3_Memory_Speed;
 
 dir.create(paste0(ReplicationFolder, '/results/PredictionAnalysis'));
 writeMat(paste0(ReplicationFolder, '/results/PredictionAnalysis/Behavior_693.mat'), 
     BBLID = BBLID, AgeYears = AgeYears, Sex = Sex, Motion = Motion,
-    F1_Exec_Comp_Res_Accuracy = F1_Exec_Comp_Res_Accuracy, F3_Memory_Accuracy = F3_Memory_Accuracy, F2_Social_Cog_Accuracy = F2_Social_Cog_Accuracy, F1_Slow_Speed = F1_Slow_Speed, F2_Fast_Speed = F2_Fast_Speed, Overall_Speed = Overall_Speed, F3_Memory_Speed = F3_Memory_Speed);
+    F1_Exec_Comp_Res_Accuracy = F1_Exec_Comp_Res_Accuracy, F3_Memory_Accuracy = F3_Memory_Accuracy, F2_Social_Cog_Accuracy = F2_Social_Cog_Accuracy);
 
