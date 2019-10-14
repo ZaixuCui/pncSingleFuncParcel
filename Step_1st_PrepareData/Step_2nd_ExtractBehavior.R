f59a9fa49ed832f3e1186777c3fca5b035036775
Zaixu Cui <zaixucui@gmail.com>
2019-10-14 11:14:09 -0400

    codes

--- Step_1st_PrepareData/Step_2nd_ExtractBehavior.R
+++ Step_1st_PrepareData/Step_2nd_ExtractBehavior.R
@@ -22,3 +22,10 @@ demo <- merge(demo, Idemo_Motion_Data, by = c("scanid", "bblid"));
 demo <- merge(demo, Cognition_Data, by = c("scanid", "bblid"));
 # Output the subjects' behavior data
 write.csv(demo, "/data/jux/BBL/projects/pncSingleFuncParcel/Replication/data/n693_Behavior_20181219.csv", row.names = FALSE);
+
+# Save age in .mat file
+BBLID = demo$bblid;
+ScanID = demo$scanid;
+Age = demo$ageAtScan1;
+writeMat('/data/jux/BBL/projects/pncSingleFuncParcel/Replication/data/Age_Info.mat', BBLID = BBLID, ScanID = ScanID, Age = Age);
+
